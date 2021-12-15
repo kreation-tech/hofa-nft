42b2b92dd53074322dd39ad417863d624808a186
Roberto Lo Giacco <rlogiacco@gmail.com>
2021-12-15 21:48:02 +0100

    improves access control support

--- src/HOFA.ts
+++ src/HOFA.ts
@@ -13,7 +13,7 @@ import { Signer } from "@ethersproject/abstract-signer";
 import { HofaNFT__factory } from "./types";
 import type { HofaNFT } from "./types";
 import addresses from "./addresses.json";
-import crypto from "crypto";
+import roles from "./roles.json";
 import { BigNumberish } from "@ethersproject/bignumber";
 
 export class HOFA {
@@ -32,8 +32,8 @@ export class HOFA {
 	}
 
 	/**
-	 * Determines the chain identifier 
-	 * 
+	 * Determines the chain identifier
+	 *
 	 * @param signerOrProvider the signer or the provider
 	 */
 	public static async getChainId(signerOrProvider: Signer | Provider): Promise<number> {
@@ -74,67 +74,6 @@ export class HOFA {
 		});
 	}
 
-	/**
-	 * Grants artist permissions to an address
-	 * 
-	 * @param address the address to grant
-	 * @param confirmations the number of confirmations to wait for, deafults to 1
-	 */
-	public async grantArtist(artist:string, confirmations:number = 1): Promise<boolean> {
-		return new Promise((resolve, reject) => { (async() => {
-			try {
-				const tx = await (await this.impl.grantRole(await this.impl.MINTER_ROLE(), artist))
-					.wait(confirmations);
-				for (const log of tx.events!) {
-					if (log.event === "RoleGranted") {
-						resolve(true);
-					}
-				}
-				resolve(false);
-			} catch (err) {
-				reject(err);
-			}
-		})();});
-	}
-
-	/**
-	 * Revokes artist permissions from an address
-	 * 
-	 * @param address the address to revoke
-	 * @param confirmations the number of confirmations to wait for, deafults to 1
-	 */
-	public async revokeArtist(artist:string, confirmations:number = 1): Promise<boolean> {
-		return new Promise((resolve, reject) => { (async() => {
-			try {
-				const tx = await (await this.impl.revokeRole(await this.impl.MINTER_ROLE(), artist))
-					.wait(confirmations);
-				for (const log of tx.events!) {
-					if (log.event === "RoleRevoked") {
-						resolve(true);
-					}
-				}
-				resolve(false);
-			} catch (err) {
-				reject(err);
-			}
-		})();});
-	}
-
-	/**
-	 * Checks if an address is listed as artist
-	 * 
-	 * @param address the address to check, defaults to current signer
-	 */
-	public async isArtist(address:string|undefined): Promise<boolean> {
-		return new Promise((resolve, reject) => { (async() => {
-			try {
-				resolve(this.impl.hasRole(await this.impl.MINTER_ROLE(), address||await (this.signerOrProvider as Signer).getAddress()));
-			} catch (err) {
-				reject(err);
-			}
-		})();});
-	}
-
 	public async metadata(title:string, description:string, uri:string, hash:string): Promise<string> {
 		return new Promise((resolve, reject) => {
 			(this.signerOrProvider as Signer).getAddress().then((address) => {
@@ -150,5 +89,124 @@ export class HOFA {
 				resolve(JSON.stringify(metadata));
 			});
 		});
+	}
+
+	/**
+	 * Grants artist permissions to an address
+	 *
+	 * @param address the address to grant
+	 * @param confirmations the number of confirmations to wait for, deafults to 1
+	 */
+	public async grantArtist(address:string, confirmations:number = 1): Promise<boolean> {
+		return this._grantRole(roles.minter, address, confirmations);
+	}
+
+	/**
+	 * Revokes artist permissions from an address
+	 *
+	 * @param address the address to revoke
+	 * @param confirmations the number of confirmations to wait for, deafults to 1
+	 */
+	public async revokeArtist(address:string, confirmations:number = 1): Promise<boolean> {
+		return this._revokeRole(roles.minter, address, confirmations);
+	}
+
+	/**
+	 * Checks if an address is listed as artist
+	 *
+	 * @param address the address to check, defaults to current signer
+	 */
+	public async isArtist(address?:string): Promise<boolean> {
+		return this._hasRole(roles.minter, address);
+	}
+
+	/**
+	 * Grants artist permissions to an address
+	 *
+	 * @param address the address to grant
+	 * @param confirmations the number of confirmations to wait for, deafults to 1
+	 */
+	public async grantAdmin(address:string, confirmations:number = 1): Promise<boolean> {
+		return this._grantRole(roles.admin, address, confirmations);
+	}
+
+	/**
+	 * Revokes artist permissions from an address
+	 *
+	 * @param address the address to revoke
+	 * @param confirmations the number of confirmations to wait for, deafults to 1
+	 */
+	public async revokeAdmin(address:string, confirmations:number = 1): Promise<boolean> {
+		return this._revokeRole(roles.admin, address, confirmations);
+	}
+
+	/**
+	 * Checks if an address is listed as admin
+	 *
+	 * @param address the address to check, defaults to current signer
+	 */
+	public async isAdmin(address?:string): Promise<boolean> {
+		return this._hasRole(roles.admin, address);
+	}
+
+	/**
+	 * Grants permissions to an address
+	 *
+	 * @param address the address to grant
+	 * @param confirmations the number of confirmations to wait for, deafults to 1
+	 */
+	private async _grantRole(role:string, address:string, confirmations:number = 1): Promise<boolean> {
+		return new Promise((resolve, reject) => { (async() => {
+			try {
+				const tx = await (await this.impl.grantRole(role, address))
+					.wait(confirmations);
+				for (const log of tx.events!) {
+					if (log.event === "RoleGranted") {
+						resolve(true);
+					}
+				}
+				resolve(false);
+			} catch (err) {
+				reject(err);
+			}
+		})();});
+	}
+
+	/**
+	 * Revokes permissions from an address
+	 *
+	 * @param address the address to revoke
+	 * @param confirmations the number of confirmations to wait for, deafults to 1
+	 */
+	private async _revokeRole(role:string, address:string, confirmations:number = 1): Promise<boolean> {
+		return new Promise((resolve, reject) => { (async() => {
+			try {
+				const tx = await (await this.impl.revokeRole(role, address))
+					.wait(confirmations);
+				for (const log of tx.events!) {
+					if (log.event === "RoleRevoked") {
+						resolve(true);
+					}
+				}
+				resolve(false);
+			} catch (err) {
+				reject(err);
+			}
+		})();});
+	}
+
+	/**
+	 * Checks if an address has been granted a role
+	 *
+	 * @param address the address to check, defaults to current signer
+	 */
+	private async _hasRole(role:string, address?:string): Promise<boolean> {
+		return new Promise((resolve, reject) => { (async() => {
+			try {
+				resolve(this.impl.hasRole(role, address || await (this.signerOrProvider as Signer).getAddress()));
+			} catch (err) {
+				reject(err);
+			}
+		})();});
 	}
 }
--- test/HOFA.test.ts
+++ test/HOFA.test.ts
@@ -3,7 +3,6 @@ import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { HofaNFT } from "../src/types";
 import { HOFA } from "../src/HOFA";
 // eslint-disable-next-line node/no-unsupported-features/node-builtins
-import { promises as fs } from "fs";
 
 const { expect } = require("chai");
 const { ethers, deployments } = require("hardhat");
@@ -25,7 +24,7 @@ describe("HOFA facade", function () {
       facade = new HOFA(artist, HofaNFT.address);
     });
 
-    it("can produce valid metadata", async function () {
+    it("Can produce valid metadata", async function () {
         expect(await facade.metadata("Piece title", "A long description", "https://ipfs.io/ipfs/bafybeigr4qpkyvzj7adu3zwvnxfctxd7sa2die3bnnu63wcfokwnxtlnja", "0x6af4b97a176f026958547cd0137600e82130bb05f971da36c7dff96655ca2ca9"))
             .to.have.string("\"name\":\"Piece title\"")
             .and.to.have.string("\"sha256\":\"0x6af4b97a176f026958547cd0137600e82130bb05f971da36c7dff96655ca2ca9\"")
@@ -34,19 +33,34 @@ describe("HOFA facade", function () {
             .and.to.have.string("\"description\":\"A long description\"");
     });
 
-    it("can grant permissions", async function () {
+    it("Can grant artists permissions", async function () {
         const admin = new HOFA(hofa, contractAddress);
+        await expect(await admin.isArtist(someone.address)).to.be.false;
         await expect(await admin.grantArtist(someone.address)).to.be.true;
+        await expect(await admin.isArtist(someone.address)).to.be.true;
         await expect(await admin.grantArtist(someone.address)).to.be.false; // if already granted returns false
     });
 
-    it("can revoke permissions", async function () {
+    it("Can revoke artist permissions", async function () {
         const admin = new HOFA(hofa, contractAddress);
+        await expect(await admin.isArtist(artist.address)).to.be.true;
         await expect(await admin.revokeArtist(artist.address)).to.be.true;
+        await expect(await admin.isArtist(artist.address)).to.be.false;
         await expect(await admin.revokeArtist(artist.address)).to.be.false; // if already revoked returns false
     });
 
-    it("can approve transfers", async function () {
+    it("Can grant and revoke admin permissions", async function () {
+        const admin = new HOFA(hofa, contractAddress);
+        await expect(await admin.isAdmin(someone.address)).to.be.false;
+        await expect(await admin.grantAdmin(someone.address)).to.be.true;
+        await expect(await admin.isAdmin(someone.address)).to.be.true;
+        await expect(await admin.revokeAdmin(someone.address)).to.be.true;
+        await expect(await admin.isAdmin(someone.address)).to.be.false;
+        await expect(await admin.revokeAdmin(someone.address)).to.be.false; // if already revoked returns false
+        await expect(await admin.isAdmin(someone.address)).to.be.false;
+    });
+
+    it("Can approve transfers", async function () {
         const tokenId = await facade.mint(
             "https://ipfs.io/ipfs/bafybeigr4qpkyvzj7adu3zwvnxfctxd7sa2die3bnnu63wcfokwnxtlnja",
             "0x6af4b97a176f026958547cd0137600e82130bb05f971da36c7dff96655ca2ca9",
