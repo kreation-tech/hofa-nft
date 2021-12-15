912389223128f9b47fe83a6a015df3c8382a01d1
Roberto Lo Giacco <rlogiacco@gmail.com>
2021-12-15 21:42:12 +0100

    renames postbuild script and adds deploy

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,17 +1,27 @@
 /* eslint-disable no-process-exit */
-import { ethers } from "hardhat";
+import { deployments, getChainId } from "hardhat";
+import { readFileSync, writeFileSync } from "fs";
+import { ethers } from "ethers";
 
-async function deploy(contract: string) {
-  console.log("Deploying " + contract + " in progress...");
-  const factory = await ethers.getContractFactory(contract);
-  const instance = await factory.deploy();
-  await instance.deployed();
-  console.log(contract + " deployed to: ", instance.address);
-  return instance.address;
+const { get } = deployments;
+const contracts:{[name: string]: string} = {};
+const roles:{[name: string]: string} = {};
+
+async function addressOf(contract:string) {
+  const deployment = await get(contract);
+  contracts[contract] = deployment.address;
 }
 
 async function main() {
-  await deploy("HofaNFT");
+  const addresses = JSON.parse(readFileSync("./src/addresses.json", "utf-8"));
+  addresses[await getChainId()] = contracts;
+
+  await addressOf("HofaNFT");
+  writeFileSync("./src/addresses.json", JSON.stringify(addresses, null, 2), { encoding: "utf-8" });
+
+  roles.admin = await ethers.constants.HashZero;
+  roles.minter = await ethers.utils.keccak256(ethers.utils.toUtf8Bytes("MINTER_ROLE"));
+  writeFileSync("./src/roles.json", JSON.stringify(roles, null, 2), { encoding: "utf-8" });
 }
 
 main()
